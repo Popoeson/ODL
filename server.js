e07ee39cc2df87387277205c34830ee15a6f77dd
Samuel Popoola <samuelpopoola323@gmail.com>
2025-08-15 17:29:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -15,10 +15,14 @@ app.use(cors());
 app.use(express.json()); // Parse JSON bodies
 
 // ===== 3. CONNECT TO MONGODB =====
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+//mongoose.connect(process.env.MONGO_URI, {
+   //  useNewUrlParser: true,
+//    useUnifiedTopology: true
+// })
+    
+// New way (Node.js Driver 4.x or Mongoose 6.x+):
+mongoose.connect(MONGO_URI);
+
 .then(() => console.log('✅ MongoDB Connected'))
 .catch(err => console.error('❌ MongoDB connection error:', err));
 
